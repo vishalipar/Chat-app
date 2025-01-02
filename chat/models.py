666dffce5e9cb2a3aeec261146e8923992844d93
vishalipar <vishalipar04@gmail.com>
2025-01-02 18:42:06 +0530

    Include Tailwind and some page design

--- chat/models.py
+++ chat/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class ChatModel(models.Model):
     room_no = models.CharField(max_length= 100)
     message = models.TextField()
+    
+    def __str__(self):
+        return f'{self.room_no} {self.message}'
+    
