f27e1e2a0c13eecd1c0aea04ca7cb9315a8a36fe
vishalipar <vishalipar04@gmail.com>
2026-01-08 00:14:59 +0530

    add close_code parameter to the disconnect function

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,7 +10,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = 'chat_'+str(self.room_name)
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-    async def disconnect(self):
+    async def disconnect(self,close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
