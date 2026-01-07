7008d2eaf2c0be36ca57d0ad88782bde4fdefe8d
vishalipar <vishalipar04@gmail.com>
2026-01-08 00:13:48 +0530

    Store get_asgi_application() fuction to the variable to avoid errors and some minor changes

--- my_chatapp/asgi.py
+++ my_chatapp/asgi.py
@@ -1,18 +1,21 @@
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.consumers
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_chatapp.settings')
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-# django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_chatapp.settings")
+
+# Initialize Django ASGI application early
+django_asgi_app = get_asgi_application()
+
+import chat.consumers  # safe to import after settings are configured
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
-    "websocket": AuthMiddlewareStack(URLRouter([path('ws/chat/<int:room_name>/',chat.consumers.ChatConsumer.as_asgi())]))
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/chat/<room_name>/", chat.consumers.ChatConsumer.as_asgi()),
+        ])
+    ),
 })
